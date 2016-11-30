7570c5fb45ef00cb4c36920465ddf6f0ae53d6ec
qq1367212627 <1367212627@qq.com>
2016-12-01 01:17:28 +0800

    添加基本函数声明

--- src/com/DAO/DataCRUD.java
+++ src/com/DAO/DataCRUD.java
@@ -1,9 +1,32 @@
 package com.DAO;
 
+import com.DAO.DBUtil;
+
+
 /**
  * Created by lewis on 2016/11/30.
  * 本类提供数据的增删改查
+ *
  */
 public class DataCRUD {
 
+    public void Create(){                                          //增加
+
+    }
+
+    public void Query() {                                           //读取查询
+
+    }
+
+    public void Update(){                                           //更新
+
+    }
+
+    public void Delete(){                                            //删除
+
+    }
+
+    public void ReleaseConnection() {                                  //释放数据链接
+
+    }
 }
