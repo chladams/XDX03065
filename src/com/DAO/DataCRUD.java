87086d7e1f7156f9a05ae5905d3f49f7e61b2b7a
qq1367212627 <1367212627@qq.com>
2016-12-01 15:45:07 +0800

    完成了Query的函数功能及测试

--- src/com/DAO/DataCRUD.java
+++ src/com/DAO/DataCRUD.java
@@ -4,6 +4,8 @@ import com.DAO.DBUtil;
 
 import java.sql.*;
 
+import static java.lang.System.out;
+
 
 /**
  * Created by lewis on 2016/11/30.
@@ -11,10 +13,9 @@ import java.sql.*;
  *
  */
 public class DataCRUD {
-    private Connection connection = null;                           //数据库的连接（会话）
-    private Statement statement = null;                             //用于执行静态 SQL 语句并返回它所生成结果的对象
-    private ResultSet resultSet = null;                             //表示数据库结果集的数据表
-    private PreparedStatement preparedStatement = null;             //表示预编译的 SQL 语句的对象。
+    private static Connection connection = null;                           //数据库的连接（会话）
+    private static ResultSet resultSet = null;                             //表示数据库结果集的数据表
+    private static PreparedStatement preparedStatement = null;             //表示预编译的 SQL 语句的对象。
 
 
     public void Create(int secore ,String playerName){                               //增加
@@ -32,35 +33,41 @@ public class DataCRUD {
         }
     }
 
-    public void Query(String playerName) {                                           //读取查询
+    public ResultSet Query(String playerName) {                                           //读取查询
         String sql = "select * from rank where playername = ?";
+        connection = new DBUtil().openConnection();
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,playerName);
+            resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
     }
 
-    public void Update(int secore , String playerName){                              //更新
+    public static void Update(int secore , String playerName){                              //更新
         String sql = "";
     }
 
-    public void Delete(String playerName){                                            //删除
+    public static void Delete(String playerName){                                            //删除
         String sql = "";
     }
 
-    public void ReleaseConnection() {                                                 //释放数据链接
+    public static void ReleaseConnection() {                                                 //释放数据链接
         try {
             if(connection!=null) {
                 connection.close();
                 connection = null;
             }
-            if(statement!=null){
-                statement.close();
-                statement = null;
-            }
-            if(resultSet!=null){
-                resultSet.close();
-                resultSet = null;
-            }
             if(preparedStatement!=null){
                 preparedStatement.close();
                 preparedStatement = null;
+            }
+            if(resultSet!=null){
+                resultSet.close();
+                resultSet=null;
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -68,6 +75,25 @@ public class DataCRUD {
     }
 
     public static void main(String []args){
-         new DataCRUD().Create(0,"admin");
+
+//      1.test Create
+//      new DataCRUD().Create(0,"admin");
+
+//       2. test Query
+//       ResultSet rs = new DataCRUD().Query("admin");
+//        try {
+//            while(rs.next()){
+//                int s = rs.getInt(1);
+//                String n = rs.getString(2);
+//                out.printf("%d %s\n",s,n);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            ReleaseConnection();
+//        }
+
+
+        
     }
 }
