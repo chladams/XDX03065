390283f576fe08a8b5dc701e6a6b299111a7e076
qq1367212627 <1367212627@qq.com>
2016-12-10 20:31:59 +0800

    完成页面解析的类的设计

--- src/com/Crawl/HtmlParserTool.java
+++ src/com/Crawl/HtmlParserTool.java
@@ -1,5 +1,13 @@
 package com.Crawl;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  * Created by lewis on 2016/12/9.
  *
@@ -7,9 +15,32 @@ package com.Crawl;
  */
 public class HtmlParserTool {
 
-    public static String Parser(String html){
+    public static ArrayList<String> Parser(String html){
+        html = DownLoadFile.downloadHtml(html);
+        Document dom =  Jsoup.parse(html);            //public static Document parse(String html, String baseUri)
+        Elements paras = dom.getElementsByClass("para");        //class选择器,获取div为para的内容
 
-        return null;
+        StringBuffer sb = new StringBuffer();                   //作为中间变量保存字符串
+
+        for(Element link : paras){
+            sb.append(link);
+        }
+
+        dom = Jsoup.parse(sb.toString());                        //再次解析出字符串
+        paras = dom.select("a[href]");                 //选择标签为a，属性含有href的元素
+
+        ArrayList<String> links = new ArrayList<>();
+
+        for(Element link:paras){
+            links.add("http://baike.baidu.com"+link.attr("href"));  //抽取出超链接
+        }
+        return links;
     }
 
+    public static void main(String []args){
+        ArrayList<String> links = Parser("http://baike.baidu.com/item/清华大学");
+        for(String link:links){
+            System.out.println(link);
+        }
+    }
 }
