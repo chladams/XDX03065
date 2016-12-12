5e5462942558e57b19128c75e62b9f6355ab4e8a
qq1367212627 <1367212627@qq.com>
2016-12-12 10:54:05 +0800

    完成了ManagerCrawl的设计

--- src/com/Crawl/ManagerCrawler.java
+++ src/com/Crawl/ManagerCrawler.java
@@ -17,29 +17,26 @@ import static java.lang.System.out;
  */
 public class ManagerCrawler {
     public static BloomFilter unique = new BloomFilter();                                    //为超链接去重
-    public static ExecutorService crawlPool = Executors.newFixedThreadPool(10);   //线程池维护页面的抓取
-    public static Queue<Result> results= new LinkedList<>();                                //结果队列
+    public static ExecutorService crawlPool = Executors.newFixedThreadPool(20);   //线程池维护页面的抓取
+    public static Queue<Result> resultsQueue= new LinkedList<>();                                //结果队列
     public static Result queueTop = new Result();                                            //队首
     public static Crawler crawler = null;                                                   //爬虫类
     public static int count = 0;
     public static final int MAX_PAGE = 1000;
 
-
     public ManagerCrawler initial(String url){                                                        //由一个url初始化爬虫
         HtmlParserTool parserTool = new HtmlParserTool();
-        results.add(parserTool.getResult(url));
+        resultsQueue.add(parserTool.getResult(url));
         unique.add(url);
         return this;
     }
 
-
-
     public void start(){
 
-        while(results.size()>0 && count<MAX_PAGE){
-            queueTop = results.poll();                                              //取出队头，并且pop
-            DataCRUD.persistent(queueTop);                                          //持久化到数据库
-            ArrayList< FutureTask<Result> > tasks = new ArrayList<>();               //为每个链接分出一个线程去运行
+        while(resultsQueue.size()>0 && count<MAX_PAGE){
+            queueTop = resultsQueue.poll();                                              //取出队头，并且pop
+            DataCRUD.persistent(queueTop);                                              //持久化到数据库
+            ArrayList< FutureTask<Result> > tasks = new ArrayList<>();                  //为每个链接分出一个线程去运行
 
             for(String link:queueTop.getUrlLink()) {                                    //扩展队头
                 if(!unique.contains(link)) {                                            //若没有爬取过这个页面
@@ -49,17 +46,19 @@ public class ManagerCrawler {
                 }
             }
 
-            for(FutureTask<Result> task:tasks){                                     //线程池执行数组的线程
+            for(FutureTask<Result> task:tasks){                                         //线程池执行数组的线程
                 crawlPool.submit(task);
-                count++;
-                if(count>MAX_PAGE){
-                    break ;
-                }
+
             }
 
             for(FutureTask<Result> task:tasks) {
                 try {
-                    out.println(task.get(2, TimeUnit.SECONDS).getTitle());  //获取线程的结果,设置每个线程的超时时间为1s
+                    Result taskResult = task.get(2, TimeUnit.SECONDS);      //获取线程的结果,设置每个线程的超时时间为1s
+                    if(!taskResult.getTitle().equals("")){                            //过滤掉误爬的链接
+                        //out.println(taskResult.getTitle());
+                        resultsQueue.add(taskResult);
+                        count++;
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (ExecutionException e) {
@@ -71,7 +70,7 @@ public class ManagerCrawler {
 
         }
         crawlPool.shutdown();                                                   //完成所有的线程，关闭线程池
-
+      //out.println(count);
     }
 
     public static void main(String []args){
